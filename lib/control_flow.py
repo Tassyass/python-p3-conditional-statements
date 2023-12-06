4e3a54ad430a7e99598622bea0510daa57bdaef8
Tassyass <tasniim@DESKTOP-5THH7BR>
2023-12-06 14:14:40 +0300

    completed

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,37 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+   password= "12345"
+print(f"Access granted")
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+   if temperature == "33":
+      print(f"Brisk !")
+   if temperature == "99":
+      print (f"Too dang hot")
+   if temperature == "75":
+      print (f"Perfect !")
+         
+hows_the_weather("33")
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        print (f"FizzBuzz")
+    if num % 3 == 0 :
+         print (f"Fizz")
+    if num % 5 == 0 :
+        print (f"Buzz") 
+
+fizzbuzz(9)           
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == "+":
+      print(num1 + num2)
+    if operation == "-":
+       print(num1 - num2)
+    if operation == "*":
+        print(num1 * num2)
+    if operation == "/":
+        print(num1 / num2)
+
+calculator("+", 2, 5)                      
